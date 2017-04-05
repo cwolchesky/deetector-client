fd333ba6a8041d0d590bd50c4a1b74193f5a2750
Chris Wolchesky <chris@wolchesky.net>
2017-04-05 14:37:58 -0400

    Adding some debugging.  Also added ENV switching.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var request = require('request');
 var config = require('./config.json');
 
+
+var ENV = process.argv.forEach(function (val, index, array) {
+    if (val.substring(0,4) == 'ENV=') {
+        var mode = val.substring(4, val.length+1);
+        console.log("Setting Environment to %s", mode);
+    }
+});
+
+
 function check_api() {
     var success = false;
     request('http://' + config.API_HOST + ":" + config.API_PORT, function(error, response, body) {
+        if (ENV == "DEV") {
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+            console.log('body:', body);
+        }
         if (response.statusCode == 200) {
         success = true;
         }
@@ -14,7 +28,7 @@ function check_api() {
 if (check_api() == true) {
     console.log("Found API at http://" + config.API_HOST + ":" + config.API_PORT);
 } else {
-    throw new Error("Did not find API!");
+    throw new Error("Did not find API at http://" + config.API_HOST + ":" + config.API_PORT +"!");
 }
 
 module.exports.check_api = check_api;
